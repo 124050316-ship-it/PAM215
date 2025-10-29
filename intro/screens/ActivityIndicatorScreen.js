8c10d90828529616e4dbed2bd37a8c17a90f3e93
124050316-ship-it <124050316@upq.edu.mx>
2025-10-28 20:28:02 -0600

    ActivityIndicatior

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,49 +1,54 @@
+import { Text, StyleSheet, View, Button, ActivityIndicator} from 'react-native'
 import React, { useState } from 'react';
-import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
 
-export default function Prueba() {
-  const [loading, setLoading] = useState(false);
+export default function ActivityIndicatorScreen(){
+ const [loading, setLoading] = useState(false);
 
-  const startLoading = () => {
-    setLoading(true);
-    // Simula una carga de 3 segundos
-    setTimeout(() => setLoading(false), 3000);
-  };
-
-  // Si loading es true, renderizamos solo el loader
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <View style={styles.loaderContainer}>
-          <ActivityIndicator size="large" color="#2196F3" animating={true} hidesWhenStopped={true} />
-          <Text>Cargando...</Text>
-        </View>
-      </View>
-    );
-  }
-
-  // Si loading es false, mostramos el estado listo
+ const startLoading = () => {
+  setLoading(true);
+  setTimeout(()=> setLoading(false), 3000)
+};
+if (loading){
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>ActivityIndicator</Text>
-      <Button title="Cargar datos" onPress={startLoading} />
-      <Text>Listo</Text>
+  <View style={styles.container}>
+    <View style={styles.loaderContainer}>
+      <ActivityIndicator
+      size="large"
+      color="#8e0707ff"
+      animating={true}
+      hidesWhenStopped={true}
+      />
+      <Text style={styles.texto}>Cargando...</Text>
     </View>
+  </View>
   );
+}
+return (
+  <View style={styles.container}>
+    <Text style={styles.title}>ActivityIndicator</Text>
+    <Button color='red'title="Carga de datos" onPress={startLoading}/>
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
+   container:{
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  loaderContainer: {
-    alignItems: 'center',
-  },
+    backgroundColor: '#000000ff',
+   },
+   texto:{
+    color: '#ffffffff',
+   },
+   title:{
+    fontSize:20,
+    alignItems:'center',
+    justifyContent:'center',
+    marginBottom:20,
+    color:'#ffffffff',
+   },
+   loaderContainer:{
+    alignItems:'center',
+   },
 });
