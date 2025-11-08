f83a56f4e7a45e55801a21824e9c9a35926daed0
124050316-ship-it <124050316@upq.edu.mx>
2025-11-07 19:50:49 -0600

    Primer comit

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Buttom, ImageBackground, ScrollView } from 'react-native';
 
 export default function App() {
+
+  const scroollRef =useRef();
+  const irALFinal = () => {
+    scroollRef.current.scrollToEnd({animated: true});
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView ref={scroollRef} style={styles.container} contentContainerStyle={styles.content}showsVerticalScrollIndicator={true}>
+        <Text>Gestion de tareas</Text>
+      <View>
+        <Buttom  color='blue' title='Ir al final' onPress={irALFinal}/>
+      </View>
       <StatusBar style="auto" />
+    <View style={styles.elemento}>
+      <Text style={styles.texto}></Text>
     </View>
+    <View style={styles.elemento}>
+      <Text style={styles.texto}></Text> 
+    </View>
+    <View style={styles.elemento}>
+      <Text style={styles.texto}></Text>
+    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content:{
+    padding:20,
+    paddingBottom:40,
+  },
+  elemento:{
+    width:'100%',
+    height:100,
+    backgroundColor: '#e6f08bff',
+    justifyContent:'center',
+    alignItems:'center',
+    marginVertical:10,
+    borderRadius:10,
+  },
+  titulo: {
+    fontSize:26,
+    fontWheight:'bold',
+    marginTop:20,
+    marginBottom:10,
+    textAlign:'center',
+    color:'fffffff',
+  },
+    texto:{
+    fontSize:16,
+    fontFamily:'Courier',
+    color:'#fcfcfcff',
+    fontWeight:'900',
+    textDecorationLine:'underline'
   },
 });
