ee210d52f0d5510c3b00d6a5dad10b3641111520
124050316-ship-it <124050316@upq.edu.mx>
2025-11-23 13:02:09 -0600

    Practica No.17 Bottom Tabs Navigator & Stack

--- tabsNavigation/App.js
+++ tabsNavigation/App.js
@@ -1,12 +1,34 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 import { Ionicons } from '@expo/vector-icons';
 
-import Home from './screens/home';
-import Profile from './screens/profile';
-import Settings from './screens/settings';
+
+import Home from './screens/home.js';
+import ProfileScreen from './screens/profile.js'; 
+import Settings from './screens/settings.js';
+import Detail from './screens/Detail.js'; 
 
 const Tab = createBottomTabNavigator();
+const ProfileStack = createNativeStackNavigator(); 
+
+
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerShown: false, 
+      }}
+    >
+      
+      <ProfileStack.Screen name="ProfileMain" component={ProfileScreen} />
+      
+    
+      <ProfileStack.Screen name="Detail" component={Detail} /> 
+    </ProfileStack.Navigator>
+  );
+}
+
 
 export default function App() {
   return (
@@ -19,11 +41,11 @@ export default function App() {
             let iconName;
 
             if (route.name === 'Home') {
-              iconName = 'home';
+              iconName = 'home-outline';
             } else if (route.name === 'Profile') {
-              iconName = 'person';
+              iconName = 'person-outline';
             } else if (route.name === 'Settings') {
-              iconName = 'settings';
+              iconName = 'settings-outline';
             }
 
             return <Ionicons name={iconName} size={size} color={color} />;
@@ -37,8 +59,10 @@ export default function App() {
         })}
       >
         <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
-        <Tab.Screen name="Settings" component={Settings} />
+        
+        <Tab.Screen name="Profile" component={ProfileStackScreen} /> 
+        
+        <Tab.Screen name="settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- tabsNavigation/screens/profile.js
+++ tabsNavigation/screens/profile.js
@@ -1,13 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native'; 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function Profile() {
+export default function Profile({ navigation }) {
+  const handleDetailsPress = () => {
+    navigation.navigate('Detail'); 
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.iconRow}>
+      <View style={styles.iconRow}> 
         <Ionicons name="person-outline" size={28} color="green" />
-        <Text style={styles.title}>Perfil de usuario</Text>
+        <Text style={styles.title}>Perfil usuario</Text>
       </View>
+      <View style={{ marginTop: 20 }}>
+          <Button 
+              title="Detalles de Usuario" 
+              onPress={handleDetailsPress} 
+              color="#007BFF"
+          />
+      </View>
+
     </View>
   );
 }
@@ -17,17 +29,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center',     
     padding: 20,
   },
   iconRow: {
-    flexDirection: 'column',
+    flexDirection: 'column', 
     alignItems: 'center',
+    marginBottom: 20, 
   },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
-    marginLeft: 10,
+    marginTop: 5,
     color: 'green',
   },
 });
